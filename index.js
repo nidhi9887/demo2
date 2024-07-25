1712d1d8cb3e8824f1fce3a1848a87c3806c3944
nidhi <nidhipal18@gmail.com>
2024-07-25 16:50:05 +0530

    files

---
+++ index.js
@@ -0,0 +1,16 @@
+const a = 5 ;
+const b = 6;
+console.log(a+b);
+let x = 5;
+x--;
+let z = x;
+++z;
+console.log(x);
+
+console.log(z);
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part);
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const reversed = months.toReversed();
+console.log(reversed);
